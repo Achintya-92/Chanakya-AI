6ab76499a1c6d855c9e401720366a4170846e2b8
Achintya-92 <lovejot4656@gmail.com>
2026-06-14 07:21:27 +0530

    updating environment variables

--- backend/src/services/systemGenerator.js
+++ backend/src/services/systemGenerator.js
@@ -10,7 +10,7 @@ export default async function SystemGenerator({
       currentState,
       availableTime,}){
 
-const apiKey = process.env.VITE_OPENROUTER_API_KEY;
+const apiKey = process.env.OPENROUTER_API_KEY;
     try{
  const res =await fetch(`https://openrouter.ai/api/v1/chat/completions`,
         {
