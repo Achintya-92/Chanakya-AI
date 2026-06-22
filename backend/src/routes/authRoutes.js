9932793031ac13062453bff96a4550cd577a5199
Achintya-92 <lovejot4656@gmail.com>
2026-06-22 17:31:24 +0530

    removing resend api functionality

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
-import { emailSend,resendOTP,verifyOTP} from "../controllers/emailController.js";
+// import { emailSend,resendOTP,verifyOTP} from "../controllers/emailController.js";
 import {verify} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -8,10 +8,10 @@ const router = express.Router();
 router.post("/register",verify, register);
 router.post("/login",verify, login);
 
-router.post("/send", emailSend);
+// router.post("/send", emailSend);
 
-router.post("/verify-email",verifyOTP);
+// router.post("/verify-email",verifyOTP);
 
-router.post("/resend-otp", resendOTP);
+// router.post("/resend-otp", resendOTP);
 
 export default router;
